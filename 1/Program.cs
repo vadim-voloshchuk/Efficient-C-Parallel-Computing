0b5b2cfedd81a6d727d00bd2a802f8ccb9d177a4
Vadim Voloschuk <vvoloschuk@irbis-tech.ru>
2023-10-26 15:24:42 +0300

    fix first part

--- 1/Program.cs
+++ 1/Program.cs
@@ -18,51 +18,87 @@ class Program
             worksheet.Cells[1, 2].Value = "Время выполнения (мс) - Последовательное";
             worksheet.Cells[1, 3].Value = "Время выполнения (мс) - Многопоточное";
 
+            double x = 12345.6789;
+
+            Console.WriteLine("\nПоследовательная обработка\n__________________________________________");
             for (int i = 1; i <= numIterations; i++)
             {
-                double x = 12345.6789;
+                char symbol = (char)('A' + i - 1); // Определение символа в зависимости от i
 
                 // Выполнение последовательно
                 DateTime t1 = DateTime.Now;
 
                 for (int j = 0; j < i; j++)
                 {
-                    for (int k = 0; k < 10000; k++)
-                    {
-                        x = Math.Sqrt(x);
-                        x = x + 0.000000001;
-                        x = Math.Pow(x, 2);
+                    for (int l = 0; l < 10000; l++)
+                            for (int s = 0; s < 10000; s++)
+                            {
+                                x = Math.Sqrt(x);
+                                x = x + 0.000000001;
+                                x = Math.Pow(x, 2);
                     }
                 }
 
                 DateTime t2 = DateTime.Now;
                 double elapsedMillisecondsSequential = (t2 - t1).TotalMilliseconds;
 
-                // Выполнение многопоточно
-                x = 12345.6789; // Сброс переменной
-                DateTime t3 = DateTime.Now;
-                Parallel.For(0, i, index =>
-                {
-                    for (int j = 0; j < 10000; j++)
-                    {
-                        for (int k = 0; k < 10000; k++)
-                        {
-                            x = Math.Sqrt(x);
-                            x = x + 0.000000001;
-                            x = Math.Pow(x, 2);
-                        }
-                    }
-                });
-
-                DateTime t4 = DateTime.Now;
-                double elapsedMillisecondsParallel = (t4 - t3).TotalMilliseconds;
+                Console.WriteLine($"{symbol}: {elapsedMillisecondsSequential} ms");
 
                 worksheet.Cells[i + 1, 1].Value = i;
                 worksheet.Cells[i + 1, 2].Value = elapsedMillisecondsSequential;
-                worksheet.Cells[i + 1, 3].Value = elapsedMillisecondsParallel;
             }
 
-            package.SaveAs(new FileInfo("Результаты.xlsx"));
-        }
+            x = 12345.6789; // Сброс переменной
+            
+            Console.WriteLine("\nПараллельная обработка\n__________________________________________");
+
+            Parallel.For(1, numIterations, index =>
+            {
+                char symbol = (char)('A' + index - 1); // Определение символа в зависимости от i
+                DateTime t3 = DateTime.Now;
+                for (int j = 0; j < index; j++){
+                        for (int l = 0; l < 10000; l++)
+                            for (int s = 0; s < 10000; s++)
+                            {
+                                x = Math.Sqrt(x);
+                                x = x + 0.000000001;
+                                x = Math.Pow(x, 2);
+                            }
+                }
+                DateTime t4 = DateTime.Now;
+                double elapsedMillisecondsParallel = (t4 - t3).TotalMilliseconds;
+                Console.WriteLine($"{symbol}: {elapsedMillisecondsParallel} ms");
+                worksheet.Cells[index + 1, 3].Value = elapsedMillisecondsParallel;
+            });
+            x = 12345.6789; // Сброс переменной
+            ExecuteCodeInParallel(numIterations, x);
+
+
+                package.SaveAs(new FileInfo("Результаты.xlsx"));
+            }
+    }
+    static void ExecuteCodeInParallel(int numIterations, double value)
+    {
+        Console.WriteLine("\nСтатика\n__________________________________________");
+        Parallel.For(1, numIterations, index =>
+        {
+                char symbol = (char)('A' + index - 1);
+                DateTime t3 = DateTime.Now;
+                for (int j = 0; j < index; j++){
+
+                        for (int l = 0; l < 10000; l++)
+                            for (int s = 0; s < 10000; s++)
+                            {
+                                value = Math.Sqrt(value);
+                                value = value + 0.000000001;
+                                value = Math.Pow(value, 2);
+                            }
+                        
+                }
+                DateTime t4 = DateTime.Now;
+                double elapsedMillisecondsParallel = (t4 - t3).TotalMilliseconds;
+                Console.WriteLine($"{symbol}: {elapsedMillisecondsParallel} ms");
+                // worksheet.Cells[index + 1, 3].Value = elapsedMillisecondsParallel;
+            });
     }
 }
